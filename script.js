d48ebbeee4332918c8f52ade5259fd8e386b3b76
Gargi Srivastava <gargisrivastava5@gmail.com>
2023-08-03 08:15:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+
+// Get necessary elements
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
+const searchResults = document.getElementById('search-results');
+const fav =  document.getElementById('fav');
+const reset = document.getElementById('reset');
+// Initialize an empty array to store fav meals
+const favoritesList = [];
+
+//take value from user and fetch in api
+searchButton.addEventListener('click', () => {
+  const searchQuery = searchInput.value;
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.meals === null) {
+        alert('No meals found for this search query.');
+      } else {
+        displaySearchResults(data.meals);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+// function to display meal items on screen
+function displaySearchResults(meals) {
+  searchResults.innerHTML = '';
+ 
+  
+  meals.forEach(meal => {
+    const mealElement = document.createElement('div');
+    mealElement.classList.add('meal');
+
+    const mealImage = document.createElement('img');
+    mealImage.src = meal.strMealThumb;
+    mealImage.alt = meal.strMeal;
+    mealElement.appendChild(mealImage);
+
+    const mealName = document.createElement('h2');
+    mealName.innerText = meal.strMeal;
+    mealElement.appendChild(mealName);
+
+    const mealIngredients = document.createElement('ul');
+    for (let i = 1; i <= 20; i++) {
+      if (meal[`strIngredient${i}`]) {
+        const ingredient = document.createElement('li');
+        ingredient.innerText = `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`;
+        mealIngredients.appendChild(ingredient);
+      } 
+      
+    }
+
+    mealElement.appendChild(mealIngredients);
+
+    const mealDescription = document.createElement('p');
+    mealDescription.classList.add('description');
+    mealDescription.innerText = meal.strInstructions;
+    mealElement.appendChild(mealDescription);
+
+    const readMoreButton = document.createElement('button');
+    readMoreButton.innerHTML = 'Read More';
+    readMoreButton.addEventListener('click', () => {
+      mealDescription.classList.toggle('show');
+    });
+    mealElement.appendChild(readMoreButton);
+
+    const favoriteButton = document.createElement('button');
+    favoriteButton.innerHTML = '<i class="fas fa-heart"></i> Add to Favorites';
+    favoriteButton.addEventListener('click', () => {
+      addToFavorites(meal);
+    });
+    mealElement.appendChild(favoriteButton);
+
+    searchResults.appendChild(mealElement);
+  });
+}
+
+
+// function to add a meal in favorites
+function addToFavorites(meal) {
+  if (!favoritesList.includes(meal)) {
+    favoritesList.push(meal);
+  }
+}
+
+fav.addEventListener('click', showFav);
+
+
+// function to show all fav meal 
+function showFav() {
+  searchResults.innerHTML = '';
+  
+  if (favoritesList.length === 0) {
+    const noFavMessage = document.createElement('p');
+    noFavMessage.innerText = 'Nothing!! Add your favourites ';
+    searchResults.appendChild(noFavMessage);
+  } else {
+    favoritesList.forEach(meal => {
+      const mealElement = document.createElement('div');
+      mealElement.classList.add('meal');
+
+      const mealImage = document.createElement('img');
+      mealImage.src = meal.strMealThumb;
+      mealImage.alt = meal.strMeal;
+      mealElement.appendChild(mealImage);
+
+      const mealName = document.createElement('h2');
+      mealName.innerText = meal.strMeal;
+      mealElement.appendChild(mealName);
+
+      const mealIngredients = document.createElement('ul');
+      for (let i = 1; i <= 20; i++) {
+          if (meal[`strIngredient${i}`]) {
+              const ingredient = document.createElement('li');
+              ingredient.innerText = `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`;
+              mealIngredients.appendChild(ingredient);
+          } else {
+              break;
+          }
+      }
+      mealElement.appendChild(mealIngredients);
+
+      const removeButton = document.createElement('button');
+      removeButton.innerHTML = '<i class="fas fa-trash"></i> Remove from Favorites';
+      removeButton.addEventListener('click', () => {
+          removeFromFavorites(meal);
+      });
+      mealElement.appendChild(removeButton);
+
+      searchResults.appendChild(mealElement);
+    });
+  }
+}
+
+// function to remove meal from favourites
+function removeFromFavorites(meal) {
+  const mealIndex = favoritesList.findIndex(fav => fav.idMeal === meal.idMeal);
+  if (mealIndex !== -1) {
+    favoritesList.splice(mealIndex, 1);
+    showFav();
+  }
+}
